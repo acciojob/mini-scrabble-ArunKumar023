39db41629bec8e9ab8ef959b7c0a15a17160b09b
Arun kumar <106857719+ArunKumar023@users.noreply.github.com>
2023-04-25 07:00:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+const evaluatedText = document.querySelector('#evaluatedText');
+const letterCount = document.querySelector('#letterCount');
+
+evaluatedText.addEventListener('input', () => {
+  const count = evaluatedText.value.length;
+  letterCount.textContent = count;
+});
