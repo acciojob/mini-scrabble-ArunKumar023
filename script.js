5034f2946596c455041ca0f547c780c058b967d8
Arun kumar <106857719+ArunKumar023@users.noreply.github.com>
2023-05-01 13:07:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-
-const evaluatedText = document.getElementById('evaluatedText');
-const letterCount = document.getElementById('letterCount');
+let evaluatedText = document.getElementById("evaluatedText");
+let letterCount = document.getElementById("letterCount");
 
 evaluatedText.addEventListener('input', () => {
-	let str = evaluatedText.value; 
-	letterCount.innerHTML = `${str.lenght}`;
+	let str = evaluatedText.value;
+	letterCount.innerHTML = `${str.length}`;
 });
